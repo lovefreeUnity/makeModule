6ced9bf1dcfae35a383a0d01d7a0c734fbf9e37b
lovefreeUnity <jo-ro3430@naver.com>
2023-02-15 18:31:00 +0900

    remove graph 필요 없는 부분 을 지웠습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,9 @@
 
 import 'package:flutter/material.dart';
-import 'package:grapth/exercise_ratio.dart';
 import 'package:grapth/res/everex_theme.dart';
-import 'patient_ftsummary.dart';
-
-
-List<String> dateList = [
-  '07.01',
-  '07.02',
-  '07.03',
-  '07.04',
-  '07.05',
-  '07.06',
-  '07.07',
-  '07.08',
-  '07.09',
-  '07.10',
-  '07.11',
-  '07.12',
-  '07.13',
-  '07.14',
-];
-List<int> exerciseRatioDataList = [
-  40,
-  58,
-  62,
-  64,
-  82,
-  79,
-  82,
-  12,
-  18,
-  30,
-  31,
-  70,
-  71,
-  100,
-];
-List<Map<String,dynamic>> dataList = [];
-addDataList() {
-  dataList.addAll(List.generate(
-      exerciseRatioDataList.length,
-          (index) => {
-        'date': dateList[index],
-        'value': exerciseRatioDataList[index].toDouble()
-      }));
-}
 
 void main() {
   moraText.setTextStyle();
-  addDataList();
   runApp(MyApp());
 }
 
@@ -65,8 +19,7 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ExerciseRatio(dataList: dataList,),
-              PatientFtSummaryPage(dataList: dataList,)
+
             ],
           ),
         ),
