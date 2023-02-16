3a5ba728968150cbdcdad344dc45a06cb02f1f85
lovefreeUnity <jo-ro3430@naver.com>
2023-02-16 09:24:24 +0900

    TextField 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:grapth/res/everex_theme.dart';
 
 void main() {
@@ -14,8 +14,61 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container()
-      ),
+          body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Center(
+            child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '휴대폰 번호',
+              style: moraText.fontSize16.copyWith(fontWeight: FontWeight.w500),
+            ),
+            Theme(
+              data: ThemeData(
+                inputDecorationTheme: InputDecorationTheme(
+                  filled: true,
+                  fillColor: MORAColor.white,
+                  contentPadding: EdgeInsets.fromLTRB(16, 12, 16, 12),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(
+                      color: MORAColor.gray4,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    borderSide: BorderSide(
+                      color: MORAColor.mintColorSub,
+                    ),
+                  ),
+                ),
+              ),
+              child: TextField(
+                autofocus: true,
+                showCursor: true,
+                keyboardType: TextInputType.phone,
+                keyboardAppearance: Brightness.light,
+                // controller: TextEditingController(
+                //   text: '',
+                // ),
+
+                //텍스트 가리기
+                // obscureText: true,
+                // obscuringCharacter: '*',
+
+                decoration: InputDecoration(
+                  suffix :Icon(Icons.circle,size: 12,),
+                  hintText: "-없이 숫자만 입력",
+                  hintStyle: moraText.fontSize16.copyWith(color: MORAColor.gray3,),
+                ),
+              ),
+            )
+            // EditableText(controller: controller, focusNode: focusNode, style: style, cursorColor: cursorColor, backgroundCursorColor: backgroundCursorColor)
+          ],
+        )),
+      )),
     );
   }
 }
