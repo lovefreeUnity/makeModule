2a31c47e37ecab3ccd83ec4fdbfa317c64d6463f
lovefreeUnity <jo-ro3430@naver.com>
2023-02-17 17:36:58 +0900

    controller에 바텀 시트 disable 추가

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -17,68 +17,77 @@ class _LoginPageState extends State<LoginPage> {
   final TextFieldController _textFieldController = TextFieldController();
 
   @override
+  void initState() {
+    _textFieldController.setController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        color: MORAColor.white,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 64,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-              child: Text(
-                '반가워요!\n'
-                '휴대폰 번호를 입력해주세요',
-                style:
-                    moraText.fontSize26.copyWith(fontWeight: FontWeight.w700),
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          color: MORAColor.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 64,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 40, 16, 63),
-              child: PhoneNumberTextField(
-                autoFocus: true,
-                hintText: '-없이 숫자만 입력',
-                textFieldLabel: '휴대폰 번호',
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  LengthLimitingTextInputFormatter(11),
-                ],
-                errorText: null,
-                textFieldController: _textFieldController,
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomSheet: Padding(
-        padding:
-            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            InkWell(
-              child: Container(
-                width: double.infinity,
-                color: MORAColor.primaryColor,
-                padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
-                child: Center(
-                  child: Text(
-                    widget.bottomSheetText,
-                    style: moraText.fontSize18.copyWith(
-                        color: MORAColor.white, fontWeight: FontWeight.w700),
-                  ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Text(
+                  '반가워요!\n'
+                  '휴대폰 번호를 입력해주세요',
+                  style:
+                      moraText.fontSize26.copyWith(fontWeight: FontWeight.w700),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 40, 16, 63),
+                child: PhoneNumberTextField(
+                  autoFocus: true,
+                  hintText: '-없이 숫자만 입력',
+                  textFieldLabel: '휴대폰 번호',
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(11),
+                  ],
+                  errorText: null,
+                  textFieldController: _textFieldController,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        bottomSheet: _textFieldController.disableBottomSheet
+            ? SizedBox()
+            : Padding(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    InkWell(
+                      child: Container(
+                        width: double.infinity,
+                        color: MORAColor.primaryColor,
+                        padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+                        child: Center(
+                          child: Text(
+                            widget.bottomSheetText,
+                            style: moraText.fontSize18.copyWith(
+                                color: MORAColor.white,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
     );
   }
 }
--- lib/textfeild_controller/textfield_controller.dart
+++ lib/textfeild_controller/textfield_controller.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 
 class TextFieldController extends TextEditingController{
+  late bool _disableBottomSheet;
+  bool get disableBottomSheet => _disableBottomSheet;
 
+  setController(){
+    _disableBottomSheet = true;
+  }
 }
