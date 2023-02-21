1364aa31e89298b2c2891e50b14c3ae9eb94d39c
lovefreeUnity <jo-ro3430@naver.com>
2023-02-21 16:50:38 +0900

    remove all 텍스트필드 다 날리고 ui만 작업 하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:grapth/LoginPage.dart';
-import 'package:grapth/res/everex_theme.dart';
 
 void main() {
-  moraText.setTextStyle();
   runApp(MyApp());
 }
 
@@ -13,7 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage()
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Color(0xFFFFFFFF),
+          hintStyle:TextStyle(fontSize: 16,fontWeight: FontWeight.w400,color: Color(0xFFAEAEAE)),
+          contentPadding: EdgeInsets.fromLTRB(16, 12, 16, 12),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            borderSide: BorderSide(
+              color: Color(0xFFDDDDDD),
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            borderSide: BorderSide(
+              color: Color(0xFF07BEB8),
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              borderSide: BorderSide(color: Color(0xFFE74C3C))),
+          focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              borderSide: BorderSide(color: Color(0xFFE74C3C))),
+        )
+      ),
     );
   }
 }
