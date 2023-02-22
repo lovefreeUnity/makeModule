4b8fd43c770341598755c99819901845e8a1d93e
lovefreeUnity <jo-ro3430@naver.com>
2023-02-22 16:26:18 +0900

    refactoring

--- lib/information_provision/information_title.dart
+++ lib/information_provision/information_title.dart
@@ -10,8 +10,8 @@ class InformationProvisionTitle extends StatelessWidget {
       required this.therapistName});
 
   TextStyle titleTextStyle = TextStyle(fontSize: 24,fontWeight: FontWeight.w700,color: Color(0xFF000000),height: 1.5);
-  TextStyle gray1 = TextStyle(fontSize: 14,fontWeight: FontWeight.w400,color:Color(0xFF545454),height: 1.5);
-  TextStyle gray3 =TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFFAEAEAE),height: 1.5,);
+  TextStyle gray1Color = TextStyle(fontSize: 14,fontWeight: FontWeight.w400,color:Color(0xFF545454),height: 1.5);
+  TextStyle gray3Color =TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Color(0xFFAEAEAE),height: 1.5,);
 
   String userName;
   String hospitalName;
@@ -33,7 +33,7 @@ class InformationProvisionTitle extends StatelessWidget {
           TextSpan(children: [
             TextSpan(
               text: hospitalName,
-              style: gray1,
+              style: gray1Color,
             ),
             WidgetSpan(
                 child: SizedBox(
@@ -41,13 +41,13 @@ class InformationProvisionTitle extends StatelessWidget {
             )),
             TextSpan(
               text: '배정일',
-              style: gray3,
+              style: gray3Color,
             ),
             WidgetSpan(
                 child: SizedBox(
               width: 8,
             )),
-            TextSpan(text: assignmentDate, style: gray1)
+            TextSpan(text: assignmentDate, style: gray1Color)
           ]),
         ),
 
@@ -59,23 +59,23 @@ class InformationProvisionTitle extends StatelessWidget {
           TextSpan(children: [
             TextSpan(
               text: '담담의',
-              style: gray3,
+              style: gray3Color,
             ),
             WidgetSpan(
                 child: SizedBox(
               width: 8,
             )),
-            TextSpan(text: doctorName, style: gray1),
+            TextSpan(text: doctorName, style: gray1Color),
             WidgetSpan(
                 child: SizedBox(
               width: 8,
             )),
-            TextSpan(text: '담당치료사', style: gray3),
+            TextSpan(text: '담당치료사', style: gray3Color),
             WidgetSpan(
                 child: SizedBox(
               width: 8,
             )),
-            TextSpan(text: therapistName, style: gray1)
+            TextSpan(text: therapistName, style: gray1Color)
           ]),
         ),
 
@@ -87,13 +87,13 @@ class InformationProvisionTitle extends StatelessWidget {
           TextSpan(children: [
             TextSpan(
               text: '주의사항',
-              style: gray3,
+              style: gray3Color,
             ),
             WidgetSpan(
                 child: SizedBox(
               width: 8,
             )),
-            TextSpan(text: '운동 전후 스트레칭을 해주세요.', style: gray1)
+            TextSpan(text: '운동 전후 스트레칭을 해주세요.', style: gray1Color)
           ]),
         )
       ],
