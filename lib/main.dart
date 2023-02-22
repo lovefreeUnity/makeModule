80ce5b54e610081bd9ad3cbba1f2581457dbf3a7
lovefreeUnity <jo-ro3430@naver.com>
2023-02-22 14:52:10 +0900

    폰트 적용

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Pretendard'),
       home: Scaffold(
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
