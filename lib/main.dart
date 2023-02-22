0bf70a308d16ec435fcfa4968e874f84a252e981
lovefreeUnity <jo-ro3430@naver.com>
2023-02-22 13:49:56 +0900

    택스트 필드 띄우기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grapth/text_field_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,31 +12,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        inputDecorationTheme: const InputDecorationTheme(
-          filled: true,
-          fillColor: Color(0xFFFFFFFF),
-          hintStyle:TextStyle(fontSize: 16,fontWeight: FontWeight.w400,color: Color(0xFFAEAEAE)),
-          contentPadding: EdgeInsets.fromLTRB(16, 12, 16, 12),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            borderSide: BorderSide(
-              color: Color(0xFFDDDDDD),
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            borderSide: BorderSide(
-              color: Color(0xFF07BEB8),
-            ),
-          ),
-          errorBorder: OutlineInputBorder(
+        fontFamily: 'Pretendard',
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: Color(0xFFFFFFFF),
+            hintStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                height: 1.5,
+                color: Color(0xFFAEAEAE)),
+            contentPadding: EdgeInsets.fromLTRB(16, 12, 16, 12),
+            enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(8)),
-              borderSide: BorderSide(color: Color(0xFFE74C3C))),
-          focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Color(0xFFDDDDDD),
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.all(Radius.circular(8)),
-              borderSide: BorderSide(color: Color(0xFFE74C3C))),
-        )
+              borderSide: BorderSide(
+                color: Color(0xFF07BEB8),
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                borderSide: BorderSide(color: Color(0xFFE74C3C))),
+            focusedErrorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                borderSide: BorderSide(color: Color(0xFFE74C3C))),
+          ),
       ),
+      home: TextFieldPage(),
     );
   }
 }
