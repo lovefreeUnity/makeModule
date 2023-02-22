0922f820082906cca34185fd16b55c9277abad14
lovefreeUnity <jo-ro3430@naver.com>
2023-02-22 13:49:01 +0900

    택스트 필드 그리기

---
+++ lib/text_field_page.dart
@@ -0,0 +1,202 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class TextFieldPage extends StatelessWidget {
+  const TextFieldPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+        color: Color(0xFFFFFFFF),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 108,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //휴대폰 번호 텍스트 필드
+                  Text(
+                    '휴대폰 번호',
+                    style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Pretendard',
+                        height: 1.5),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        hintText: '-없이 숫자만 입력',
+                        suffixIcon: Icon(Icons.abc_rounded),
+                        suffixIconColor: Color(0xFFDDDDDD)),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              //인증 번호 텍스트 필드
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      hintText: '인증번호를 입력해주세요.',
+                      suffixIcon: Icon(Icons.abc_rounded),
+                      suffixIconColor: Color(0xFFDDDDDD),
+                    ),
+                  ),
+                  //에러 텍스트
+                  Text(
+                    '인증번호를 다시 확인해주세요.',
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xFfE74C3C),
+                        fontWeight: FontWeight.w400,
+                        height: 1.5),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+
+              //이름 입력란
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '이름',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Color(0xFF000000),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Pretendard',
+                        height: 1.5),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      hintText: '실명 입력',
+                      suffixIcon: Icon(Icons.abc_rounded),
+                      suffixIconColor: Color(0xFFDDDDDD),
+                    ),
+                  ),
+                ],
+              ),
+
+              //주의 사항
+              Row(
+                children: [
+                  Icon(
+                    Icons.add_a_photo,
+                    size: 14,
+                  ),
+                  Text(
+                    '잘못 입력한 경우 운동 배정이 되지 않으니 주의해주세요.',
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xFF07BEB8),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Pretendard',
+                        height: 1.5),
+                  )
+                ],
+              ),
+
+              SizedBox(height: 20),
+
+              //주민등록번호
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('주민등록번호',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Pretendard',
+                          height: 1.5)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            hintText: '생년월일',
+                            suffixIcon: Icon(Icons.abc_rounded),
+                            suffixIconColor: Color(0xFFDDDDDD),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                        child: Container(
+                          width: 7,
+                          height: 1,
+                          color: Color(0xFF545454),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            hintText: '0',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+
+              //비밀 번호
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('비밀번호',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Pretendard',
+                          height: 1.5)),
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: '영문, 숫자, 특수문자의 조합 (8-16자)',
+                        suffixIcon: Icon(Icons.add)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: Container(
+        padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+        color: Color(0xFFDDDDDD),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "다음",
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFFFFFFFF),
+                  height: 1.5),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
