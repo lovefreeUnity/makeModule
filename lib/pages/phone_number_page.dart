23466c359d4a1af13e8e473eeda7d5c6b7a6b083
lovefreeUnity <jo-ro3430@naver.com>
2023-02-24 17:07:46 +0900

    전화번호 입력 페이지에 레드의 validator_builder 적용 완료

--- lib/pages/phone_number_page.dart
+++ lib/pages/phone_number_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:grapth/validator_builder.dart';
 
 class PhoneNumberPage extends StatefulWidget {
@@ -9,10 +10,13 @@ class PhoneNumberPage extends StatefulWidget {
 }
 
 class _PhoneNumberPageState extends State<PhoneNumberPage> {
-
   final validate = ValidationBuilder().phone().build();
   TextEditingController textEditingController = TextEditingController();
-  
+
+  String errorText = '';
+  bool showIcon = false;
+  int maxTextLength = 11;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,11 +26,18 @@ class _PhoneNumberPageState extends State<PhoneNumberPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('반가워요!\n'
-                '휴대폰 번호를 입력해주세요',
-              style: TextStyle(color: Color(0xFF000000),fontSize: 26,fontWeight: FontWeight.w700,height: 1.5),
+            Text(
+              '반가워요!\n'
+              '휴대폰 번호를 입력해주세요',
+              style: TextStyle(
+                  color: Color(0xFF000000),
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700,
+                  height: 1.5),
             ),
-            SizedBox(height: 40,),
+            SizedBox(
+              height: 40,
+            ),
             Text(
               '휴대폰 번호',
               style: TextStyle(
@@ -39,30 +50,65 @@ class _PhoneNumberPageState extends State<PhoneNumberPage> {
             // TextFormField(),
             TextField(
               controller: textEditingController,
-              keyboardType: TextInputType.phone,
-              onChanged: (text){setState(() {
-
-              });},
+              keyboardType: TextInputType.number,
+              onChanged: (text) {
+                textEditingController.text.isEmpty
+                    ? showIcon = false
+                    : showIcon = true;
+                setState(() {});
+              },
               decoration: InputDecoration(
                   hintText: '-없이 숫자만 입력',
-                  suffixIcon: textEditingController.text.isEmpty ?null: InkWell(onTap: (){
-                    textEditingController.clear();
-                  }, child: Icon(Icons.abc_rounded)),
-                  suffixIconColor:Color(0xFFDDDDDD)),
-              onTapOutside: (pointerDownEvent){
+                  errorText: errorText.isEmpty ? null : '',
+                  suffixIcon: showIcon
+                      ? InkWell(
+                          onTap: () {
+                            errorText = '';
+                            textEditingController.clear();
+                            showIcon = false;
+                            setState(() {
+
+                            });
+                          },
+                          child: Icon(Icons.abc_rounded))
+                      : null,
+                  suffixIconColor: Color(0xFFDDDDDD)),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(maxTextLength)
+              ],
+              onTapOutside: (pointerDownEvent) {
                 FocusScope.of(context).unfocus();
               },
+            ),
+            Text(
+              errorText,
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Color(0xFfE74C3C),
+                  fontWeight: FontWeight.w400,
+                  height: 1.5),
             )
           ],
         ),
       ),
       bottomSheet: InkWell(
-        onTap: (){
-          Navigator.pushNamed(context , '/AuthenticationNumber');
+        onTap: () {
+          if (textEditingController.text.length == maxTextLength) {
+            if (validate(textEditingController.text)!.isNotEmpty) {
+              errorText = validate(textEditingController.text)!;
+              setState(() {});
+            } else {
+              Navigator.pushNamed(context, '/AuthenticationNumber');
+            }
+          }
         },
+        //git commit -m '전화번호 입력 페이지에 레드의 validator_builder 적용 완료'
         child: Container(
             padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
-            color: false ? Color(0xFF07BEB8) :Color(0xFFDDDDDD),
+            color: textEditingController.text.length == maxTextLength
+                ? Color(0xFF07BEB8)
+                : Color(0xFFDDDDDD),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -75,8 +121,7 @@ class _PhoneNumberPageState extends State<PhoneNumberPage> {
                       height: 1.5),
                 ),
               ],
-            )
-        ),
+            )),
       ),
     );
   }
