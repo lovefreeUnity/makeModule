a1fb7edb2a2968d89b3ec6827d7c48d563aa8df7
lovefreeUnity <jo-ro3430@naver.com>
2023-02-28 11:02:41 +0900

    resizeToAvoidBottomInset가 true일 시 바텀시트의 높이에 대해서는 받고 있지않아 포커스된 텍스트 필드를 가리게 된다. 우선 컨테이너에 magin을 주어 resizeToAvoidBottomInset 가 true일 때와 비슷한 효과를 주었다.다만 드래그하기 충분한 높이가 있지않다면 가려지는 위젯이 생긴다.

--- lib/pages/phone_number_page.dart
+++ lib/pages/phone_number_page.dart
@@ -18,93 +18,105 @@ class _PhoneNumberPageState extends State<PhoneNumberPage> {
   int maxTextLength = 11;
 
   @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xFFFFFFFF),
-        padding: EdgeInsets.fromLTRB(16, 64, 16, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 64,),
-            Text(
-              '반가워요!\n'
-              '휴대폰 번호를 입력해주세요',
-              style: TextStyle(
-                  color: Color(0xFF000000),
-                  fontSize: 26,
-                  fontWeight: FontWeight.w700,
-                  height: 1.5),
+      resizeToAvoidBottomInset: false,
+        body: Container(
+          color: Color(0xFFFFFFFF),
+          height: double.infinity,
+          margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 64,
+                  ),
+                  Text(
+                    '반가워요!\n'
+                        '휴대폰 번호를 입력해주세요',
+                    style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 26,
+                        fontWeight: FontWeight.w700,
+                        height: 1.5),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    '휴대폰 번호',
+                    style: TextStyle(
+                        color: Color(0xFF000000),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'Pretendard',
+                        height: 1.5),
+                  ),
+                  // TextFormField(),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (text) {
+                      textEditingController.text.isEmpty
+                          ? showIcon = false
+                          : showIcon = true;
+                      setState(() {});
+                    },
+                    decoration: InputDecoration(
+                        hintText: '-없이 숫자만 입력',
+                        errorText: errorText.isEmpty ? null : '',
+                        suffixIcon: showIcon
+                            ? InkWell(
+                            onTap: () {
+                              errorText = '';
+                              textEditingController.clear();
+                              showIcon = false;
+                              setState(() {});
+                            },
+                            child: Icon(Icons.abc_rounded))
+                            : null,
+                        suffixIconColor: Color(0xFFDDDDDD)),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(maxTextLength)
+                    ],
+                    onTapOutside: (pointerDownEvent) {
+                      // FocusScope.of(context).unfocus();
+                    },
+                  ),
+                  Text(
+                    errorText,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xFfE74C3C),
+                        fontWeight: FontWeight.w400,
+                        height: 1.5),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              '휴대폰 번호',
-              style: TextStyle(
-                  color: Color(0xFF000000),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Pretendard',
-                  height: 1.5),
-            ),
-            // TextFormField(),
-            TextField(
-              controller: textEditingController,
-              keyboardType: TextInputType.number,
-              onChanged: (text) {
-                textEditingController.text.isEmpty
-                    ? showIcon = false
-                    : showIcon = true;
-                setState(() {});
-              },
-              decoration: InputDecoration(
-                  hintText: '-없이 숫자만 입력',
-                  errorText: errorText.isEmpty ? null : '',
-                  suffixIcon: showIcon
-                      ? InkWell(
-                          onTap: () {
-                            errorText = '';
-                            textEditingController.clear();
-                            showIcon = false;
-                            setState(() {
-
-                            });
-                          },
-                          child: Icon(Icons.abc_rounded))
-                      : null,
-                  suffixIconColor: Color(0xFFDDDDDD)),
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-                LengthLimitingTextInputFormatter(maxTextLength)
-              ],
-              onTapOutside: (pointerDownEvent) {
-                FocusScope.of(context).unfocus();
-              },
-            ),
-            Text(
-              errorText,
-              style: TextStyle(
-                  fontSize: 12,
-                  color: Color(0xFfE74C3C),
-                  fontWeight: FontWeight.w400,
-                  height: 1.5),
-            ),
-          ],
+          )
         ),
-      ),
-      bottomSheet: InkWell(
-        onTap: () {
-          if (textEditingController.text.length == maxTextLength) {
-            if (validate(textEditingController.text) != null) {
-              errorText = validate(textEditingController.text)!;
-              setState(() {});
-            } else {
-              Navigator.pushNamed(context, '/AuthenticationNumber');
+        bottomSheet: InkWell(
+          onTap: () {
+            if (textEditingController.text.length == maxTextLength) {
+              if (validate(textEditingController.text) != null) {
+                errorText = validate(textEditingController.text)!;
+                setState(() {});
+              } else {
+                Navigator.pushNamed(context, '/AuthenticationNumber');
+              }
             }
-          }
-        },
-        child: Container(
+          },
+          child: Container(
             padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
             color: textEditingController.text.length == maxTextLength
                 ? Color(0xFF07BEB8)
@@ -121,8 +133,9 @@ class _PhoneNumberPageState extends State<PhoneNumberPage> {
                       height: 1.5),
                 ),
               ],
-            )),
-      ),
+            ),
+          ),
+        ),
     );
   }
 }
