52a8baf584fca187475bfed01d0c46c63a13dbd9
lovefreeUnity <jo-ro3430@naver.com>
2023-02-28 12:25:00 +0900

    인증번호 페이지 텍스트 초기화 아이콘 기능 추가

--- lib/pages/authentication_number.dart
+++ lib/pages/authentication_number.dart
@@ -12,7 +12,8 @@ class AuthenticationNumber extends StatefulWidget {
 class _AuthenticationNumberState extends State<AuthenticationNumber> {
   TextEditingController textEditingController = TextEditingController();
   AuthenticationNumberRestController authenticationNumberRestController =
-      AuthenticationNumberRestController();
+  AuthenticationNumberRestController();
+  bool showIcon = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,110 +24,124 @@ class _AuthenticationNumberState extends State<AuthenticationNumber> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              height: 64,
-            ),
-            Text(
-              '010-1234-5678로\n'
+          SizedBox(
+          height: 64,
+        ),
+        Text(
+          '010-1234-5678로\n'
               '인증번호를 보내드렸어요',
-              style: TextStyle(
-                  color: Color(0xFF000000),
-                  fontSize: 26,
-                  fontWeight: FontWeight.w700,
-                  height: 1.5),
-            ),
+          style: TextStyle(
+              color: Color(0xFF000000),
+              fontSize: 26,
+              fontWeight: FontWeight.w700,
+              height: 1.5),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Row(
+          children: [
+            Text('문자를 받지 못 하셨나요?',
+                style: TextStyle(
+                    color: Color(0xFF747474),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    height: 1.5)),
             SizedBox(
-              height: 4,
+              width: 16,
             ),
-            Row(
-              children: [
-                Text('문자를 받지 못 하셨나요?',
+            AnimatedBuilder(
+              animation: authenticationNumberRestController,
+              builder: (BuildContext context, Widget? child) {
+                return InkWell(
+                  onTap: () {
+                    if (authenticationNumberRestController._isEnable ==
+                        false) {
+                      //인증 번호 다시 보내기 클릭 이벤트
+                      authenticationNumberRestController.onClick();
+                    }
+                  },
+                  child: Text.rich(
+                    TextSpan(
+                        text: authenticationNumberRestController.isEnable
+                            ? '${authenticationNumberRestController
+                            .countTime}초 후 '
+                            : '',
+                        children: [TextSpan(text: '다시 받기')]),
                     style: TextStyle(
-                        color: Color(0xFF747474),
+                        color: Color(0xFF6AD8D4),
                         fontSize: 16,
                         fontWeight: FontWeight.w400,
-                        height: 1.5)),
-                SizedBox(
-                  width: 16,
-                ),
-                AnimatedBuilder(
-                  animation: authenticationNumberRestController,
-                  builder: (BuildContext context, Widget? child) {
-                    return InkWell(
-                      onTap: () {
-                        if(authenticationNumberRestController._isEnable == false){
-                          //인증 번호 다시 보내기 클릭 이벤트
-                          authenticationNumberRestController.onClick();
-                        }
-                      },
-                      child: Text.rich(
-                        TextSpan(
-                            text: authenticationNumberRestController.isEnable
-                                ? '${authenticationNumberRestController.countTime}초 후 '
-                                : '',
-                            children: [TextSpan(text: '다시 받기')]),
-                        style: TextStyle(
-                            color: Color(0xFF6AD8D4),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            height: 1.5),
-                      ),
-                    );
-                  },
-                )
-              ],
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            TextField(
-              controller: textEditingController,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                hintText: '인증번호를 입력해주세요.',
-                suffixIcon: textEditingController.text.isEmpty
-                    ? null
-                    : Icon(Icons.abc_rounded),
-                suffixIconColor: Color(0xFFDDDDDD),
-              ),
-              onTapOutside: (pointerDownEvent) {
-                FocusScope.of(context).unfocus();
+                        height: 1.5),
+                  ),
+                );
               },
-            ),
-            //에러 텍스트
-            Text(
-              '인증번호를 다시 확인해주세요.',
-              style: TextStyle(
-                  fontSize: 12,
-                  color: Color(0xFfE74C3C),
-                  fontWeight: FontWeight.w400,
-                  height: 1.5),
             )
           ],
         ),
-      ),
-      bottomSheet: InkWell(
-        onTap: () {
-          Navigator.pushNamed(context, '/TermsOfServicePage');
-          // Navigator.pushNamed(context , '/AlreadySignedUpAccountPage');
+        SizedBox(
+          height: 40,
+        ),
+        TextField(
+          controller: textEditingController,
+          keyboardType: TextInputType.phone,
+          onChanged: (text) {
+            textEditingController.text.isEmpty
+                ? showIcon = false
+                : showIcon = true;
+            setState(() {});
+          },
+          decoration: InputDecoration(
+              hintText: '인증번호를 입력해주세요.',
+              suffixIcon: showIcon
+                  ? InkWell(
+                onTap: () {
+                  textEditingController.clear();
+                  showIcon = false;
+                  setState(() {});
+                },
+                child: Icon(Icons.abc_rounded,color: Color(0xFFDDDDDD),),
+              ) : null,
+              suffixIconColor: Color(0xFFDDDDDD),
+        ),
+        onTapOutside: (pointerDownEvent) {
+          FocusScope.of(context).unfocus();
         },
-        child: Container(
-            padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
-            color: Color(0xFFDDDDDD),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "다음",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFFFFFFFF),
-                      height: 1.5),
-                ),
-              ],
-            )),
       ),
+      //에러 텍스트
+      Text(
+        '인증번호를 다시 확인해주세요.',
+        style: TextStyle(
+            fontSize: 12,
+            color: Color(0xFfE74C3C),
+            fontWeight: FontWeight.w400,
+            height: 1.5),
+      )
+      ],
+    ),
+    ),
+    bottomSheet: InkWell(
+    onTap: () {
+    Navigator.pushNamed(context, '/TermsOfServicePage');
+    // Navigator.pushNamed(context , '/AlreadySignedUpAccountPage');
+    },
+    child: Container(
+    padding: EdgeInsets.fromLTRB(0, 16, 0, 16),
+    color: Color(0xFFDDDDDD),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text(
+    "다음",
+    style: TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: Color(0xFFFFFFFF),
+    height: 1.5),
+    ),
+    ],
+    )),
+    ),
     );
   }
 }
